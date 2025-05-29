f8fdb27983c3f4fa0b6db11ef2110c215541128b
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-05-29 17:18:24 +0300

    improve js execution example

--- src/test/java/guru/qa/tests/JSExecutionTest.java
+++ src/test/java/guru/qa/tests/JSExecutionTest.java
@@ -5,10 +5,13 @@ import com.codeborne.selenide.Selenide;
 import guru.qa.TestBase;
 import guru.qa.pages.RegistrationPage;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class JSExecutionTest extends TestBase {
@@ -21,9 +24,13 @@ public class JSExecutionTest extends TestBase {
         registrationPage
                 .openPage();
 
-        String jsScript = Files.readString(Path.of("src/test/resources/js/alert.js1"));
+        String jsScript = Files.readString(Path.of("src/test/resources/js/alert.js"));
 
         Selenide.executeJavaScript(jsScript);
+
+        Alert alert = Selenide.switchTo().alert();
+
+        assertEquals("Test alert message", alert.getText());
 
         Selenide.confirm();
 
